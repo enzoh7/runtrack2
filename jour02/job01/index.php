20e406108ce0e323700c764049f4795a197251af
enzoh7 <enzo.henry@laplateforme.io>
2025-09-17 11:17:33 +0200

    .

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -9,16 +9,12 @@
     <h1>Job 01 - Affichage des nombres de 0 à 1337</h1>
     
     <?php
-    // Boucle pour afficher tous les nombres de 0 à 1337
     for ($i = 0; $i <= 1337; $i++) {
-        // Condition spéciale pour le nombre 42
         if ($i == 42) {
             echo "<b><u>" . $i . "</u></b>";
         } else {
             echo $i;
         }
-        
-        // Ajouter un retour à la ligne après chaque nombre
         echo "<br />";
     }
     ?>
